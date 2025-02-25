f1ff103a76352f795023a01f80cc11c47111b66e
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2025-02-25 21:11:47 +0530

    completed transaction

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -8,12 +8,14 @@ const app = express();
 app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
+  const body = req.body;
 
   //store in db a new trigger
   await client.$transaction(async (tx) => {
     const run = await client.zapRun.create({
       data: {
         zapId: zapId,
+        metadata: body
       },
     });
     await client.zapRunOutbox.create({
