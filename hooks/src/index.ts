eed25af0e19ca6dccbb31c94c6a2ada6550e2b98
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2025-02-25 20:59:44 +0530

    transaction

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,9 +1,27 @@
-import express from 'express'
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const app = express();
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) =>{
-    const userId = req.params.userId;
-    const zapId = req.params.zapId;
-    
-})
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+
+  //store in db a new trigger
+  await client.$transaction(async (tx) => {
+    const run = await client.zapRun.create({
+      data: {
+        zapId: zapId,
+      },
+    });
+    await client.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
+
+  //
+});
