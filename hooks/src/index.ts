cc3d16ee366f84cf693fe1d37f1e313faf42e76f
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2025-02-26 21:24:24 +0530

    hooks complete

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient();
 
 const app = express();
+app.use(express.json())
 
 app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const userId = req.params.userId;
