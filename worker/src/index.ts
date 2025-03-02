d60459033c3d898352f1084fb1550e733aec1e6f
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2025-03-02 11:44:34 +0530

    complete worker

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -14,13 +14,24 @@ async function main() {
   await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
 
   await consumer.run({
+    autoCommit: false,
     eachMessage: async ({ topic, partition, message }) => {
       console.log({
         partition,
         offset: message.offset,
         value: message.value?.toString(),
       });
-      await new Promise((r) => setTimeout(r, 1000));
+      //stop the excution for 1 second
+      await new Promise((r) => setTimeout(r, 5000));
+
+      console.log("processing done!");
+      await consumer.commitOffsets([
+        {
+          topic: TOPIC_NAME,
+          partition: 0,
+          offset: (parseInt(message.offset) + 1).toString(),
+        },
+      ]);
     },
   });
 }
